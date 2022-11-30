fc224cdad61e714279813263de9fc683be2c5abe
pedroMontandon <pedrobarbosa89@gmail.com>
2022-11-30 18:04:25 -0300

    exerícios 8.2 feitos

---
+++ Fundamentos/8-Hof/02-Sort-Map/exercicio.js
@@ -0,0 +1,83 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+// Exercício 1: Organizar a Biblioteca
+let bookIndex = [];
+let authorBirthYear = [];
+let sortedAuthors = [];
+
+books.map((element) => {
+    bookIndex.push(`${element.name} - ${element.genre} - ${element.author.name}`)
+    authorBirthYear.push(`${element.author.name} - ${element.author.birthYear}`)
+})
+
+function sortingAuthors() {
+    books.map((element) => {
+        sortedAuthors.push({author: element.author.name,
+        age: element.releaseYear - element.author.birthYear})
+    })
+    sortedAuthors.sort((element, element2) => element.age - element2.age);
+}
+sortingAuthors();
+
+
